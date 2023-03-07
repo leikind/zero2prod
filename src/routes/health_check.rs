43e6e872dca09326ed39b4bd48ddbf64db404b04
Yuri Leikind <yuri.leikind@meeco.me>
2023-03-07 12:30:28 +0100

    config

--- src/routes/health_check.rs
+++ src/routes/health_check.rs
@@ -1,5 +1,7 @@
 use actix_web::HttpResponse;
 
+// curl -i  --request GET 'http://localhost:8000/health_check'
+
 pub async fn health_check() -> HttpResponse {
   // println!("health_check!!!");
   HttpResponse::Ok().finish()
--- src/routes/subscriptions.rs
+++ src/routes/subscriptions.rs
@@ -6,6 +6,8 @@ pub struct FormData {
   name: String,
 }
 
+// curl -i  --request POST 'http://localhost:8000/subscriptions' -d "email=foo%40gmail.com&name=Peter"
+
 pub async fn subscribe(form: web::Form<FormData>) -> HttpResponse {
   println!("subscribe {} {}", form.email, form.name);
   HttpResponse::Ok().finish()
