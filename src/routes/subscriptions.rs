1b33700ce091c4cabb7f958f4a4203529d37e2ed
Yuri Leikind <yuri.leikind@meeco.me>
2023-03-14 13:24:02 +0100

    Chapter 3 finished

--- src/routes/subscriptions.rs
+++ src/routes/subscriptions.rs
@@ -1,5 +1,7 @@
 use actix_web::{web, HttpResponse};
-use sqlx::PgConnection;
+use chrono::Utc;
+use sqlx::PgPool;
+use uuid::Uuid;
 
 #[derive(serde::Deserialize)]
 pub struct FormData {
@@ -9,10 +11,26 @@ pub struct FormData {
 
 // curl -i  --request POST 'http://localhost:8000/subscriptions' -d "email=foo%40gmail.com&name=Peter"
 
-pub async fn subscribe(
-  form: web::Form<FormData>,
-  _connection: web::Data<PgConnection>,
-) -> HttpResponse {
+pub async fn subscribe(form: web::Form<FormData>, pool: web::Data<PgPool>) -> HttpResponse {
   println!("subscribe {} {}", form.email, form.name);
-  HttpResponse::Ok().finish()
+
+  match sqlx::query!(
+    r#"
+    insert into subscriptions (id, email, name, subscribed_at)
+    values ($1, $2, $3, $4)
+    "#,
+    Uuid::new_v4(),
+    form.email,
+    form.name,
+    Utc::now()
+  )
+  .execute(pool.get_ref())
+  .await
+  {
+    Ok(_) => HttpResponse::Ok().finish(),
+    Err(e) => {
+      println!("Failed to execute query {}", e);
+      HttpResponse::InternalServerError().finish()
+    }
+  }
 }
--- tests/health_check.rs
+++ tests/health_check.rs
@@ -1,15 +1,21 @@
-use sqlx::{Connection, PgConnection};
+use sqlx::{Connection, Executor, PgConnection, PgPool};
 use std::net::TcpListener;
-use zero2prod::configuration::get_configuration;
+use uuid::Uuid;
+use zero2prod::configuration::{get_configuration, DatabaseSettings};
+
+pub struct TestApp {
+  pub address: String,
+  pub db_pool: PgPool,
+}
 
 #[tokio::test]
 async fn health_check_works() {
-  let address = spawn_app().await;
+  let app = spawn_app().await;
 
   let client = reqwest::Client::new();
 
   let response = client
-    .get(&format!("{}/health_check", address))
+    .get(&format!("{}/health_check", app.address))
     .send()
     .await
     .expect("failed to execute request");
@@ -19,31 +25,14 @@ async fn health_check_works() {
 }
 
 #[tokio::test]
-async fn can_connect_to_db() {
-  let _app_address = spawn_app().await;
-  let configuration = get_configuration().expect("Failed to read configuration");
-  let connection_string = configuration.database.connection_string();
-
-  let _connection = PgConnection::connect(&connection_string)
-    .await
-    .expect("Failed to connect to Postgres");
-}
-
-#[tokio::test]
 async fn subscribe_returns_a_200_for_valid_data() {
-  let app_address = spawn_app().await;
-  let configuration = get_configuration().expect("Failed to read configuration");
-  let connection_string = configuration.database.connection_string();
-
-  let mut connection = PgConnection::connect(&connection_string)
-    .await
-    .expect("Failed to connect to Postgres");
-
+  let app = spawn_app().await;
   let client = reqwest::Client::new();
+
   let body = "name=yuri&email=yuri%40foo.org";
 
   let response = client
-    .post(&format!("{}/subscriptions", &app_address))
+    .post(&format!("{}/subscriptions", &app.address))
     .header("Content-Type", "application/x-www-form-urlencoded")
     .body(body)
     .send()
@@ -53,7 +42,7 @@ async fn subscribe_returns_a_200_for_valid_data() {
   assert_eq!(200, response.status().as_u16());
 
   let saved = sqlx::query!("select email, name from subscriptions")
-    .fetch_one(&mut connection)
+    .fetch_one(&app.db_pool)
     .await
     .expect("Failed to fetch subscriptions");
 
@@ -63,8 +52,7 @@ async fn subscribe_returns_a_200_for_valid_data() {
 
 #[tokio::test]
 async fn subscribe_returns_a_400_when_data_is_missing() {
-  let address = spawn_app().await;
-
+  let app = spawn_app().await;
   let client = reqwest::Client::new();
 
   let test_cases = vec![
@@ -75,7 +63,7 @@ async fn subscribe_returns_a_400_when_data_is_missing() {
 
   for (invalid_body, error_message) in test_cases {
     let response = client
-      .post(&format!("{}/subscriptions", &address))
+      .post(&format!("{}/subscriptions", &app.address))
       .header("Content-Type", "application/x-www-form-urlencoded")
       .body(invalid_body)
       .send()
@@ -91,19 +79,81 @@ async fn subscribe_returns_a_400_when_data_is_missing() {
   }
 }
 
-async fn spawn_app() -> String {
+async fn spawn_app() -> TestApp {
   let listener = TcpListener::bind("127.0.0.1:0").expect("Failed to bind address");
   let port = listener.local_addr().unwrap().port();
+  let address = format!("http://127.0.0.1:{}", port);
 
-  let configuration = get_configuration().expect("Failed to read configuration");
-  let connection = PgConnection::connect(&configuration.database.connection_string())
-    .await
-    .expect("Failed to connect to Postgres");
+  let mut configuration = get_configuration().expect("Failed to read configuration");
 
-  let server = zero2prod::startup::run(listener, connection).expect("Failed to bind address");
+  configuration.database.database_name = Uuid::new_v4().to_string();
+
+  let connection_pool = configure_database(&configuration.database).await;
+
+  let server =
+    zero2prod::startup::run(listener, connection_pool.clone()).expect("Failed to bind address");
   let _ = tokio::spawn(server);
 
   // println!("port {}", port);
 
-  format!("http://127.0.0.1:{}", port)
+  TestApp {
+    address,
+    db_pool: connection_pool,
+  }
+}
+
+pub async fn configure_database(config: &DatabaseSettings) -> PgPool {
+  let mut connection = PgConnection::connect(&config.connection_string_without_db())
+    .await
+    .expect("Failed to connect to Postgres");
+
+  let q = format!(r#"CREATE DATABASE "{}";"#, config.database_name);
+
+  connection
+    .execute(q.as_str())
+    .await
+    .expect("Failed to create database.");
+
+  let connection_pool = PgPool::connect(&config.connection_string())
+    .await
+    .expect("Failed to connect to Postgres.");
+
+  sqlx::migrate!("./migrations")
+    .run(&connection_pool)
+    .await
+    .expect("Failed to migrate the database");
+
+  connection_pool
+}
+
+// cargo test test_uuids -- --exact --nocapture
+#[test]
+fn test_uuids() {
+  use uuid::Uuid;
+
+  // let u: Uuid = Uuid::new_v4();
+  let u: Uuid = Uuid::parse_str("a1a2a3a4b1b2c1c2d1d2d3d4d5d6d7d8").expect("Failed to parse UUID");
+
+  assert_eq!(u.simple().to_string(), "a1a2a3a4b1b2c1c2d1d2d3d4d5d6d7d8");
+  assert_eq!(
+    u.hyphenated().to_string(),
+    "a1a2a3a4-b1b2-c1c2-d1d2-d3d4d5d6d7d8"
+  );
+  assert_eq!(
+    u.braced().to_string(),
+    "{a1a2a3a4-b1b2-c1c2-d1d2-d3d4d5d6d7d8}"
+  );
+  assert_eq!(
+    u.urn().to_string(),
+    "urn:uuid:a1a2a3a4-b1b2-c1c2-d1d2-d3d4d5d6d7d8"
+  );
+}
+
+// cargo test test_utc_lib -- --exact --nocapture
+#[test]
+fn test_utc_lib() {
+  use chrono::Utc;
+
+  let a = Utc::now();
+  println!("{}", a);
 }
